1646ac4c71ab46928a9b9e163da698c4cfbb0b38
Kat Jansen <kat@17cosmicln.net>
2020-03-01 21:15:58 -0600

    Reorganize scrape function, add data to dictionary. Error with .text for first twitter than nasa news.

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,6 +1,6 @@
 
 import pandas as pd
-import requests as r
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
@@ -24,12 +24,15 @@ def s_scrape(url):
     driver.close()
     return soup
 
+#Scrape provided website with requests
+#Return soup of page.
 def r_scrape(url):
     r = requests.get(jpl_search).text
     return BeautifulSoup(r,'lxml')
 
 #Put everything into a single function to return a dictionary of everything.
 def scrape():
+    data = {}
 
     #Selenium driver to get soups
     nasa_soup = s_scrape(nasa_url)
@@ -42,21 +45,27 @@ def scrape():
 
 
     ##Search through soups to find text
+    ## add information to data dictionary.
 
     #Nasa Mars Latest Article title and teaser
     news_title = nasa_soup.body.find('div',class_='content_title').text
     news_p = nasa_soup.body.find('div',class_='article_teaser_body').text
+    nasa_latest = {'title':news_title,'teaser':news_p}
+    data['nasa']=nasa_latest
 
     # Featured Image from JPL
     featured_image_url = jpl_base+jpl_soup.body.find('article')['style'].split(' ')[1].strip('url').strip(";(')")
+    data['featured_image'] = featured_image_url
 
     #Twitter
     mars_weather = twitter_soup.body.find('p',class_='TweetTextSize TweetTextSize--normal js-tweet-text tweet-text').text
+    data['mars_weather'] = mars_weather
 
     #Mars Facts Table
     facts_df = pd.read_html(str(facts_soup.body.find('table',id='tablepress-p-mars-no-2')))[0]
     facts_df.columns = ['description','value']
     facts_df = facts_df.set_index('description').to_html()
+    data['facts_table'] = facts_df
 
     #Astropedia
     #Find Title and url to hemisphere pages.
@@ -68,6 +77,9 @@ def scrape():
         soup = s_scrape(h['url'])
         img = soup.find('a',target='_blank')
         h.update({'img_url':img['href']})
+    data['hemispheres'] = astro_d
+
+    return data
 
 if __name__ == "__main__":
     scrape()
