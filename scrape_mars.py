43a563bc18a0a4f904fb0337bf2b120173248f69
Kat Jansen <kat@17cosmicln.net>
2020-03-01 21:01:27 -0600

    Put all code into a function, pull out scraping code into 2 separate functions, one for selenium and one for requests.

--- scrape_mars.py
+++ scrape_mars.py
@@ -14,58 +14,66 @@ facts_url = "http://space-facts.com/mars/"
 astro_base = 'https://astrogeology.usgs.gov'
 astro_url = astro_base+"/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
 
-#Selenium driver to get soups
-driver = webdriver.Firefox()
-driver.get(nasa_url)
-driver.implicitly_wait(10)
-nasa_soup = BeautifulSoup(driver.page_source,"lxml")
-
-driver.get(facts_url)
-driver.implicitly_wait(10)
-facts_soup = BeautifulSoup(driver.page_source,"lxml")
-
-driver.get(astro_url)
-driver.implicitly_wait(10)
-astro_soup = BeautifulSoup(driver.page_source,"lxml")
-
-#Requests to get remaining soups
-jpl_r = r.get(jpl_search).text
-jpl_soup = BeautifulSoup(jpl_r,'lxml')
-
-twitter_r = r.get(twitter_url).text
-twitter_soup = BeautifulSoup(twitter_r,'lxml')
-
-
-##Search through soups to find text
-
-#Nasa Mars Latest Article title and teaser
-news_title = nasa_soup.body.find('div',class_='content_title').text
-news_p = nasa_soup.body.find('div',class_='article_teaser_body').text
-
-# Featured Image from JPL
-featured_image_url = jpl_base+jpl_soup.body.find('article')['style'].split(' ')[1].strip('url').strip(";(')")
-
-#Twitter
-mars_weather = twitter_soup.body.find('p',class_='TweetTextSize TweetTextSize--normal js-tweet-text tweet-text').text
-
-#Mars Facts Table
-facts_df = pd.read_html(str(facts_soup.body.find('table',id='tablepress-p-mars-no-2')))[0]
-facts_df.columns = ['description','value']
-facts_df = facts_df.set_index('description')
-
-
-#Astropedia
-#Find Title and url to hemisphere pages.
-astro_h = astro_soup.find_all('h3')
-astro_d = [{'title':h.text,'url':astro_base+h.parent['href']} for h in astro_h]
-
-#Visit each url found above and add the img_url to the appropriate dictionary.
-for h in astro_d:
-    driver.get(h['url'])
+#Scrape provided website with selenium
+#Return soup of page.
+def s_scrape(url):
+    driver = webdriver.Firefox()
+    driver.get(nasa_url)
     driver.implicitly_wait(10)
     soup = BeautifulSoup(driver.page_source,"lxml")
-    img = soup.find('a',target='_blank')
-    h.update({'img_url':img['href']})
+    driver.close()
+    return soup
 
-#Close the Selenium Webdriver
-driver.close()
+def r_scrape(url):
+    r = requests.get(jpl_search).text
+    return BeautifulSoup(r,'lxml')
+
+#Put everything into a single function to return a dictionary of everything.
+def scrape():
+    
+    #Selenium driver to get soups
+    nasa_soup = s_scrape(nasa_url)
+    facts_soup = s_scrape(facts_url)
+    astro_soup = s_scrape(astro_url)
+
+    #Requests to get remaining soups
+    jpl_soup = r_scrape(jpl_search)
+    twitter_soup = r_scrape(twitter_url)
+
+
+    ##Search through soups to find text
+
+    #Nasa Mars Latest Article title and teaser
+    news_title = nasa_soup.body.find('div',class_='content_title').text
+    news_p = nasa_soup.body.find('div',class_='article_teaser_body').text
+
+    # Featured Image from JPL
+    featured_image_url = jpl_base+jpl_soup.body.find('article')['style'].split(' ')[1].strip('url').strip(";(')")
+
+    #Twitter
+    mars_weather = twitter_soup.body.find('p',class_='TweetTextSize TweetTextSize--normal js-tweet-text tweet-text').text
+
+    #Mars Facts Table
+    facts_df = pd.read_html(str(facts_soup.body.find('table',id='tablepress-p-mars-no-2')))[0]
+    facts_df.columns = ['description','value']
+    facts_df = facts_df.set_index('description')
+
+
+    #Astropedia
+    #Find Title and url to hemisphere pages.
+    astro_h = astro_soup.find_all('h3')
+    astro_d = [{'title':h.text,'url':astro_base+h.parent['href']} for h in astro_h]
+
+    #Visit each url found above and add the img_url to the appropriate dictionary.
+    for h in astro_d:
+        driver.get(h['url'])
+        driver.implicitly_wait(10)
+        soup = BeautifulSoup(driver.page_source,"lxml")
+        img = soup.find('a',target='_blank')
+        h.update({'img_url':img['href']})
+
+    #Close the Selenium Webdriver
+    driver.close()
+
+if __name__ == "__main__":
+    scrape()
