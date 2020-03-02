a9b766310a0af7ed24a04d9d5121e7cb16db9f48
Kat Jansen <kat@17cosmicln.net>
2020-03-01 21:03:15 -0600

    Create empty routes.

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return
+
+@app.route('/scrape')
+def scrape():
+    return
+
+
 if __name__ == "__main__":
     app.run(debug=True)
