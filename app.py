7d3cca4ae6701d72e275cf496cf20ca5db32eb59
Kat Jansen <kat@17cosmicln.net>
2020-03-03 18:17:58 -0600

    define routes

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, jsonify, request
+from scrape_mars import scrape
+import pymongo
+
 app = Flask(__name__)
+
+CONN = "mongodb://localhost:27017"
+client = pymongo.MongoClient(CONN)
+db = client.mars_db
 
 @app.route('/')
 def home():
-    return
+    result = db.mars_collection.find_one()
+    return render_template("index.html",result=result)
 
 @app.route('/scrape')
-def scrape():
+def scrape_route():
+    result = scrape()
+    db.mars_collection.insert_one(result)
     return
 
 
